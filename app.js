91cdb5de447edf0818c3e1eb2f50329108a64ece
Micheal <michealobarewon2015@gmail.com>
2026-01-25 21:06:16 +0100

    feat: added joi and logger

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const logger = require('./middleware/logger');
+const {
+  createTodoValidator,
+  editTodoValidator,
+} = require('./middleware/validator.js');
+
+const errorHandler = require('./middleware/errorHandler');
+const e = require('express');
 const app = express();
 app.use(express.json()); // Parse JSON bodies
 
+app.use(logger); // Custom middleware
 let todos = [
   { id: 1, task: 'Learn Node.js', completed: false },
   { id: 2, task: 'Build CRUD API', completed: false },
@@ -13,14 +22,14 @@ app.get('/todos', (req, res) => {
 });
 
 // POST New – Create
-app.post('/todos', (req, res) => {
+app.post('/todos', createTodoValidator, (req, res) => {
   const newTodo = { id: todos.length + 1, ...req.body }; // Auto-ID
   todos.push(newTodo);
   res.status(201).json(newTodo); // Echo back
 });
 
 // PATCH Update – Partial
-app.patch('/todos/:id', (req, res) => {
+app.patch('/todos/:id', editTodoValidator, (req, res) => {
   const todo = todos.find((t) => t.id === parseInt(req.params.id)); // Array.find()
   if (!todo) return res.status(404).json({ message: 'Todo not found' });
   Object.assign(todo, req.body); // Merge: e.g., {completed: true}
@@ -29,12 +38,16 @@ app.patch('/todos/:id', (req, res) => {
 
 // DELETE Remove
 app.delete('/todos/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const initialLength = todos.length;
-  todos = todos.filter((t) => t.id !== id); // Array.filter() – non-destructive
-  if (todos.length === initialLength)
-    return res.status(404).json({ error: 'Not found' });
-  res.status(204).send(); // Silent success
+  try {
+    const id = parseInt(req.params.id);
+    const initialLength = todos.length;
+    todos = todos.filter((t) => t.id !== id); // Array.filter() – non-destructive
+    if (todos.length === initialLength)
+      return res.status(404).json({ error: 'Not found' });
+    res.status(204).send(); // Silent success
+  } catch (error) {
+    next(error); // Pass to error handler
+  }
 });
 
 app.get('/todos/completed', (req, res) => {
@@ -42,9 +55,7 @@ app.get('/todos/completed', (req, res) => {
   res.json(completed); // Custom Read!
 });
 
-app.use((err, req, res, next) => {
-  res.status(500).json({ error: 'Server error!' });
-});
+app.use(errorHandler); // Error-handling middleware
 
 const PORT = 3002;
 app.listen(PORT, () => console.log(`Server on port ${PORT}`));
